1264ef35759ac523e0e63c612bfc844639800454
bomauriel <Bomauriel@yahoo.com>
2022-03-31 20:26:03 -0400

    Bash

---
+++ bash/Readme.md
@@ -0,0 +1,2 @@
+Full Vid link on youtube 
+https://www.youtube.com/watch?v=SPwyp2NG-bE&t=618s&ab_channel=NetworkChuck
---
+++ bash/himom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "hi mom"
+
+sleep 3 
+
+echo "Uh uh" 
+
+sleep 3
+
+echo "oh wow"
+
+sleep 3 
+
+echo "i can't believe that"
+
+sleep 3
+
+echo "Bye"
